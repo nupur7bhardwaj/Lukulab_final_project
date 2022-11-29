eef6242ac8f594594592dd974dcc6945ff50da96
nupur7bhardwaj <111491951+nupur7bhardwaj@users.noreply.github.com>
2022-11-29 11:06:14 +0200

    test

--- lukulab/src/App.js
+++ lukulab/src/App.js
@@ -1,22 +1,24 @@
 
 import './App.css';
-import Image1 from './Components/Image1.jsx';
-import Audio from './Components/Audio.jsx';
-import Group_Button from './Components/Group_Button.jsx'
+import Images from './Components/Images.jsx';
+
+
 
 function App() {
   return (
     <>
     <div className = "container">
-      <Image1/>
-      <Audio/>
-      <button type="button" className="btn btn-outline-light">Next</button>
+    <div className="Bro">
+      <h2>EXERCISE-2</h2>
+      <br/>
+      <br/>
+      <Images/>
+      <br/>
+      <br/>
+      </div>
       <footer>
-        <p>
-        <button type="button" className="btn btn-link">How to do the exercise?</button>
-        </p>
-        <button type="button" className="btn btn-primary" disabled data-bs-toggle="button">Done</button>
-        <Group_Button/>
+       <button type="button" className="btn btn-link">How to do the exercise?</button>
+        <br/>
       </footer>
     </div>
     </>
