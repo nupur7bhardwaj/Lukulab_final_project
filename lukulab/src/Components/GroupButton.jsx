2a3d0fcd268c55e963528905737e6b887e38d63f
nupur7bhardwaj <111491951+nupur7bhardwaj@users.noreply.github.com>
2022-11-29 11:06:52 +0200

    test

---
+++ lukulab/src/Components/GroupButton.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Group_Button(){
+
+    return(
+       <> <button onClick={undefined}><span type="button" className =  "glyphicon glyphicon-ok"style = {{color: "Green"}}> </span></button>
+
+        <button onClick={undefined}><span type="button" className ="glyphicon glyphicon-remove" style = {{color: "Red"}}></span></button></>
+    )
+    }
